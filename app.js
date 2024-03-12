83edf8bd193baa9bfbc9574cb11c1d3144c75f42
samuel-lassoie <59116773+samuel-lassoie@users.noreply.github.com>
2024-03-12 20:13:09 +0100

    wip

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //import du framework express
 const express = require('express');
+// création tableau utilisateurs
+const users = []
 
 // on crée notre application
 const app = express();
@@ -29,6 +31,38 @@ app.post('', (req, res) => {
 
     res.send(`MESSAGE RECUsss: ${nom} ${prenom}`)
 })
+
+//Exercice
+// créez une route qui permet de créer des utilisateurs nom et prenom, stockez 
+// ces utilisateurs dans un tableau au niveau de l'application
+
+//Créez une router permettant d'afficher l'ensemble des utilisateurs au format JSON
+
+
+app.post('/users', (req, res) => {
+    const { nom, prenom } = req.body;
+    if(nom != null && prenom != null) {
+        users.push({
+            nom,
+            prenom
+        })
+        return res.send(`Bonjour ${nom} ${prenom}`)
+    } else {
+        res.send('informations manquantes')
+    }
+})
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+
+
+//créez une méthode permettant de supprimer un utilisateur, 
+//passez en paramètre l'index du user que vous souhaitez supprimer
+// si l'index n'existe pas retourner une erreur 404 non trouvée
+// /users?id=8        /users/8
+
 
 
 // on lance notre application
