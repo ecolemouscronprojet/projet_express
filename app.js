f960dddb082dac52f4953877b25d0074a87af79c
samuel-lassoie <59116773+samuel-lassoie@users.noreply.github.com>
2024-03-12 18:39:55 +0100

    wip

--- app.js
+++ app.js
@@ -1,175 +1,37 @@
+//import du framework express
 const express = require('express');
-const bodyParser = require('body-parser')
-const { v4: uuidv4 } = require('uuid');
 
+// on crée notre application
 const app = express();
-// database
-const database = {
-    users: []
-}
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// associer le moteur de template EJS à notre application
 app.set('view engine', 'ejs');
 
+//on déclare un port sur lequel sera lancé notre application
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-    const { prenom } = req.query
-    res.render('pages/index', { prenom });
-});
-
-
-
-// hello-world  sans passer par le TPL
-app.get('/hello-world', (req, res) => {
-    res.send('Hello world')
-})
-
-// page multiplication où vous allez créer deux variables
-// nb1 = 10
-// nb2 = 20
-// rendez la multiplication de ces deux nombres
-app.get('/multiplication', function(req, res){
-    const nb1 = req.query.nb1 ?? 1
-    const nb2 = req.query.nb2 ?? 1
-    
-    const result = nb1 * nb2
-    res.send(`Resultat ${result}`)
-})
-
-
-// créer la route table-de-multiplication
-
-// app.get('/table-de-multiplication', (req, res) => {
-//     const nb = req.query.nb ?? 1
-//     let result = ''
-//     for(let i = 1; i<=10; i++){
-//         result += `${i} * ${nb} = ${i*nb} <br>`
-//     }
-
-//     res.send(result)
-// })
-
-
-app.get('/table-de-multiplication', (req, res) => {
-    const nb = req.query.nb ?? 1
-
-    res.render('pages/table-de-multiplication' , {
-        nb: nb
+// création de la route principale permettant d'afficher un message texte
+app.get('', (req, res) => {
+    const prenom = req.query.prenom ?? 'Samuel';
+    res.render('pages/index', {
+        prenom
     })
 })
 
-app.post('/formulaire-save', (req, res) => {
-    const id = req.body.id
-    const nom = req.body.nom
-    const prenom = req.body.prenom
+// méthode permettant de poster des données
+app.post('', (req, res) => {
+    const prenom = req.body?.prenom;
+    const nom = req.body?.nom;
 
-    if(id == null){
-        database.users.push({
-            id: uuidv4(),
-            nom,
-            prenom 
-        })
-    } else {
-        // retrouver l'utilisateur
-        // mdofier nom et prenom
-    }
-    // res.send(`Bonjour ${nom} ${prenom}`)
-    res.redirect('/utilisateurs')
-})
-
-// Créer une nouvelle route /utilisateurs
-// avec un tpl html qui permet de lister les utilisateurs
-
-
-
-// CREER UN UTILISATEUR
-app.get('/formulaire', (req, res) => {
-    res.render('pages/formulaire',  {
-        user: {}
-    })
-})
-
-// MODIFIER UN UTILISATEUR
-app.get('/update-user', (req, res) => {
-    const id = req.query.id;
-    if(id === undefined) {
-        return res.redirect('/utilisateurs')
-    }
-
-    // const user = database.users.find((user) => user.id === id)
-
-    let user = null
-    for(let i=0; i < database.users.length; i++){
-        const u = database.users[i];
-        if(u.id ===id) {
-            user = u;
-        }
-    }
-
-    if(user === null) {
-        return res.redirect('/utilisateurs')
-    }
-
-
-    res.render('pages/formulaire', {
-        user
-    })
-
+    res.send(`MESSAGE RECUsss: ${nom} ${prenom}`)
 })
 
 
-
-
-app.get('/utilisateurs', (req, res) => {
-    res.render('pages/utilisateurs', {
-        users: database.users
-    })
-})
-
-
-
-// hello-world-2 création d'une page et affichage
-//               Hello world dans la page
-
-
-app.get('/bonjour', (req, res) => {
-    // const nom = req.query.nom ? req.query.nom : '-'
-    // const prenom = req.query.prenom ? req.query.prenom : '-'
-    const nom = req.query.nom ?? '-'
-    const prenom = req.query.prenom ?? '-'
-
-    res.send(`Bonjour ${nom}  ${prenom}`)
-
-});
-
-app.get('/supprimer', (req, res) => {
-    const id = req.query.id
-    if (id != null) {
-        const index = database.users.findIndex((user) => {
-            return user.id === id
-        })
-        if (index !== -1) {
-            database.users.splice(index, 1)
-        }
-    }
-
-    res.redirect('/utilisateurs')
-})
-
-
-app.get('/calculatrice', (req, res) => {
-    let { nb1, nb2 } = req.query
-    if(nb1 === undefined || nb2 === undefined) {
-        return res.send('Veuillez indiquer le nb1 et nb2 !')
-    }
-    nb1 = parseInt(nb1, 10)
-    nb2 = parseInt(nb2, 10)
-    const somme = nb1 + nb2
-    res.send(`Voici le résultat: ${somme}`)
-});
-
-app.listen(port, () => {
-    console.log('Mon APP est lancée')
+// on lance notre application
+app.listen(port, function() {
+    console.log('Application lancée')
 })
---
+++ app_old.js
@@ -0,0 +1,183 @@
+const express = require('express');
+
+
+const bodyParser = require('body-parser')
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+// database
+const database = {
+    users: []
+}
+
+app.set('view engine', 'ejs');
+
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    const { prenom } = req.query
+    res.render('pages/index', { prenom });
+});
+
+
+
+// hello-world  sans passer par le TPL
+app.get('/hello-world', (req, res) => {
+    res.send('Hello world')
+})
+
+// page multiplication où vous allez créer deux variables
+// nb1 = 10
+// nb2 = 20
+// rendez la multiplication de ces deux nombres
+app.get('/multiplication', function(req, res){
+    const nb1 = req.query.nb1 ?? 1
+    const nb2 = req.query.nb2 ?? 1
+    
+    const result = nb1 * nb2
+    res.send(`Resultat ${result}`)
+})
+
+
+// créer la route table-de-multiplication
+
+// app.get('/table-de-multiplication', (req, res) => {
+//     const nb = req.query.nb ?? 1
+//     let result = ''
+//     for(let i = 1; i<=10; i++){
+//         result += `${i} * ${nb} = ${i*nb} <br>`
+//     }
+
+//     res.send(result)
+// })
+
+
+app.get('/table-de-multiplication', (req, res) => {
+    const nb = req.query.nb ?? 1
+
+    res.render('pages/table-de-multiplication' , {
+        nb: nb
+    })
+})
+
+app.post('/formulaire-save', (req, res) => {
+    const id = req.body.id
+    const nom = req.body.nom
+    const prenom = req.body.prenom
+
+    if(id == null || id.length ===0) {
+        database.users.push({
+            id: uuidv4(),
+            nom,
+            prenom 
+        })
+    } else {
+        let user = null
+        for(let i=0; i < database.users.length; i++){
+            const u = database.users[i];
+            if(u.id ===id) {
+                user = u;
+            }
+        }
+        // LE METTRE A JOUR
+    }
+   
+    res.redirect('/utilisateurs')
+})
+
+// Créer une nouvelle route /utilisateurs
+// avec un tpl html qui permet de lister les utilisateurs
+
+
+
+// CREER UN UTILISATEUR
+app.get('/formulaire', (req, res) => {
+    res.render('pages/formulaire',  {
+        user: {}
+    })
+})
+
+// MODIFIER UN UTILISATEUR
+app.get('/update-user', (req, res) => {
+    const id = req.query.id;
+    if(id === undefined) {
+        return res.redirect('/utilisateurs')
+    }
+
+    // const user = database.users.find((user) => user.id === id)
+
+    let user = null
+    for(let i=0; i < database.users.length; i++){
+        const u = database.users[i];
+        if(u.id ===id) {
+            user = u;
+        }
+    }
+
+    if(user === null) {
+        return res.redirect('/utilisateurs')
+    }
+
+
+    res.render('pages/formulaire', {
+        user
+    })
+
+})
+
+
+
+
+app.get('/utilisateurs', (req, res) => {
+    res.render('pages/utilisateurs', {
+        users: database.users
+    })
+})
+
+
+
+// hello-world-2 création d'une page et affichage
+//               Hello world dans la page
+
+
+app.get('/bonjour', (req, res) => {
+    // const nom = req.query.nom ? req.query.nom : '-'
+    // const prenom = req.query.prenom ? req.query.prenom : '-'
+    const nom = req.query.nom ?? '-'
+    const prenom = req.query.prenom ?? '-'
+
+    res.send(`Bonjour ${nom}  ${prenom}`)
+
+});
+
+app.get('/supprimer', (req, res) => {
+    const id = req.query.id
+    if (id != null) {
+        const index = database.users.findIndex((user) => {
+            return user.id === id
+        })
+        if (index !== -1) {
+            database.users.splice(index, 1)
+        }
+    }
+
+    res.redirect('/utilisateurs')
+})
+
+
+app.get('/calculatrice', (req, res) => {
+    let { nb1, nb2 } = req.query
+    if(nb1 === undefined || nb2 === undefined) {
+        return res.send('Veuillez indiquer le nb1 et nb2 !')
+    }
+    nb1 = parseInt(nb1, 10)
+    nb2 = parseInt(nb2, 10)
+    const somme = nb1 + nb2
+    res.send(`Voici le résultat: ${somme}`)
+});
+
+app.listen(port, () => {
+    console.log('Mon APP est lancée')
+})
